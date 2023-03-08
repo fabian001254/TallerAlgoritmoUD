92513ac3147e2c7fad634e1ed0cc444fd738112b
Fabian Trujillo <facatrusa@gmail.com>
2023-03-08 16:39:23 -0500

    añadido el taller 1

---
+++ taller.js
@@ -0,0 +1,16 @@
+//Cual es el mayor de dos variables
+var a = 0, b=5;
+if(a> b){
+    console.log(a);
+}else{
+    console.log(b);
+}
+//Cual es el mayor de 3 variables
+var a=7, b=3, c=24;
+if(a> b && a> c){
+    console.log(a);
+}else if(b>a && b>c){
+    console.log(b);
+}else{
+    console.log(c);
+}
